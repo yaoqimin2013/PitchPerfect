31d6ea08959871b319fa00913fb6be7460b3db50
Qimin Yao <qiminyao.aw@gmail.com>
2016-04-23 21:21:26 -0400

    Add secondViewController

---
+++ Pitch Perfect/Pitch Perfect/PlayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlayViewController.swift
+//  Pitch Perfect
+//
+//  Created by Qimin Yao on 4/23/16.
+//  Copyright © 2016 Qimin Yao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlayViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
